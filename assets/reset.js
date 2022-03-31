4657cd1de3ba99a740f6f311965cc55e8bce6fe7
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-31 17:00:05 +0200

    Generate html page to reset password of a user

--- assets/reset.js
+++ assets/reset.js
@@ -1,18 +1,1 @@
 const axios = require("axios");
-
-function reset() {
-  let password = document.getElementById("password").value;
-  let email = document.getElementById("email").value;
-
-  axios
-    .post("http://localhost:8000/auth/reset/", {
-      email: email,
-      password: password,
-    })
-    .then(function (res) {
-      console.log(res);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-}
